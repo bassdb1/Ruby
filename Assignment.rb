a621352305566435831b7b0bd18172ec7b44c47b
bassdb1 <d-bass@outlook.com>
2021-06-25 09:45:22 -0400

    Add files via upload

---
+++ Assignment.rb
@@ -0,0 +1,8 @@
+# 1. create a variable called "number" and assign it the value 15
+number = 15
+
+# 2. add two references to the variable "number" together, assigning the expression to the variable "sum"
+sum = number + number
+
+# 3. print the variable "sum" to the console
+puts sum
