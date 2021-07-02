94eff3c81322f5645a7aecc7394e273570e49d79
bassdb1 <d-bass@outlook.com>
2021-07-02 11:09:26 -0400

    Add files via upload

---
+++ BracketNotationArray.rb
@@ -0,0 +1,5 @@
+teas = ["green", "black", "dark", "yellow", "white", "oolong"]
+# add the array index of the first item in the array
+puts teas[0]
+# add the array index of the last item in the array
+puts teas[5]
