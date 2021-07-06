5db41a712b407e1afaeac3021575762e8229e0f9
bassdb1 <d-bass@outlook.com>
2021-07-06 10:42:52 -0400

    Add files via upload

---
+++ HashBracketNotation.rb
@@ -0,0 +1,12 @@
+grace_hopper = {
+    full_name: "Grace Hopper",
+    date_of_birth: "December 9, 1906",
+    occupations: ["computer scientist", "US Navy rear admiral"],
+    achievements: ["led the team responsible for releasing some of the first compiled languages", "received more than 40 honorary degrees", "popularized the idea of programming languages written in English and compiled to code understood by computers", "and so much more!"],
+    source: "https://en.wikipedia.org/wiki/Grace_Hopper"
+  }
+  
+  # add the symbol corresponding to the achievements key in the `grace_hopper` hash
+  puts grace_hopper[:full_name]
+  puts grace_hopper[:occupations]
+  puts grace_hopper[:achievements]
